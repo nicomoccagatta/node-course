ea9962b1c0ac28c818a4876a5abe58f893a1ea0c
nicomoccagatta <nicolas.moccagatta@mercadolibre.com>
2018-09-17 11:00:25 -0300

    Exercise Country-Info

--- exercises/country-info/index.js
+++ exercises/country-info/index.js
@@ -1,11 +1,22 @@
-module.exports = {
+const fs = require("fs"),
+  file = `${__dirname}/countries.json`;
 
+function loadCountriesAndFind(finderFunc, cb) {
+  fs.readFile(file, (err, data) => {
+    if (err) {
+      return cb(err, null);
+    }
+    const result = JSON.parse(data).find(finderFunc);
+    return cb(null, result);
+  });
+}
+
+module.exports = {
   getCountryInfo(code, cb) {
-    // write your code here!
+    return loadCountriesAndFind((obj) => obj.code === code, cb);
   },
 
   getCountryInfoByName(name, cb) {
-    // write your code here!
+    return loadCountriesAndFind((obj) => obj.name === name, cb);
   }
-
 };
