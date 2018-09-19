34896c078676abdeae8ccbdab5660e6a2ac68f11
Diego Calero <dcalero@fiqus.com>
2018-09-18 22:26:38 -0300

    Completing some tests so they could be used as an example to complete the others.

--- exercises/testing/countries.tests.js
+++ exercises/testing/countries.tests.js
@@ -6,11 +6,25 @@ const countryInfo = require("../country-info"),
 describe("countries tests", () => {
 
   context("unit", () => {
+
+    const data = {
+      code: "FQ",
+      name: "Fiqus"
+    };
   
     describe("success cases", () => {
 
       it("getByCode() should call countryInfo.getCountryInfo() with given country code and return data", () => {
-        
+        const stubGetCountryInfo = sinon.stub(countryInfo, "getCountryInfo").callsFake((code, cb) => {
+          stubGetCountryInfo.restore();
+          expect(code).to.eql(data.code);
+          cb(null, data);
+        });
+
+        return countries.getByCode(data.code).then((country) => {
+          expect(stubGetCountryInfo.calledOnce).to.eql(true);
+          expect(country).to.eql(data);
+        });
       });
 
       it("getByCode() should return null when countryInfo.getCountryInfo() doesn't return a valid response", () => {
@@ -29,8 +43,22 @@ describe("countries tests", () => {
 
     describe("error cases", () => {
       
+      const error = new Error("This is an error!");
+
       it("getByCode() should reject with the error given by countryInfo.getCountryInfo()", () => {
-        
+        const stubGetCountryInfo = sinon.stub(countryInfo, "getCountryInfo").callsFake((_, cb) => {
+          stubGetCountryInfo.restore();
+          cb(error);
+        });
+
+        return countries.getByCode(data.code)
+          .then(() => {
+            throw new Error("This promise should be rejected!");
+          })
+          .catch((err) => {
+            expect(stubGetCountryInfo.calledOnce).to.eql(true);
+            expect(err).to.eql(error);
+          });
       });
 
       it("getByName() should reject with the error given by countryInfo.getCountryInfoByName()", () => {
@@ -44,7 +72,9 @@ describe("countries tests", () => {
   context("integration", () => {
 
     it("getByCode() should get the name of the country 'AR'", () => {
-      
+      return countries.getByCode("AR").then((country) => {
+        expect(country.name).to.eql("Argentina");
+      });
     });
 
     it("getByCode() should return null when a country is not found", () => {
